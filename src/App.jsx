8faccf981aa93164917d9da3278cf81fb3c70ea8
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-08-02 10:57:19 +0100

    add a loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ function App() {
   const [joke, setJoke] = useState('');
   const [delivery, setDelivery] = useState('');
   const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false); // Corrected spelling
-
+  const [loading, setLoading] = useState(false);
   const fetchJoke = async () => {
     const apiUrl = 'https://v2.jokeapi.dev/joke/Any';
     try {
-      setLoading(true); // Set loading to true before fetching
+      setLoading(true); 
       const response = await fetch(apiUrl);
       if (!response.ok) {
         setError('Network response was not ok');
-        setLoading(false); // Ensure loading is set to false on error
+        setLoading(false);
         return;
       }
       const data = await response.json();
@@ -26,18 +25,20 @@ function App() {
         setJoke(data.joke); 
         setDelivery(''); 
       }
-      setLoading(false); // Set loading to false after data is fetched
+      setLoading(false); 
     } catch (error) {
       console.error('Error fetching the data:', error);
       setError('Failed to fetch joke. Please try again later.');
-      setLoading(false); // Ensure loading is set to false on error
+      setLoading(false); 
     }
   };
 
   useEffect(() => {
     fetchJoke();
   }, []);
-
+if(loading){
+  return <p>Loading...</p>
+}
   return (
     <div>
       <h1>Random Joke</h1>
